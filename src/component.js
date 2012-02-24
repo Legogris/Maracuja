a472f507576fa39da25f1c955d28eb9b6081ad7a
Legogris <legogris@legogris.se>
2012-02-24 17:51:06 +0100

    adding components to entities with init

--- src/component.js
+++ src/component.js
@@ -1,30 +1,30 @@
 var Component = function(MC) {
-return function(_id, attrs, ancestorIDs) {
-	var id = _id;	
-	var ancestors = [];
+	return function(_id, attrs, ancestorIDs) {
+		var id = _id;	
+		var ancestors = [];
 
-	var getID = function() {
-		return id;
+		var getID = function() {
+			return id;
+		};
+
+		//CONSTRUCTOR
+		this.getID = getID;
+		this.ancestors = ancestors;
+		this.attrs = attrs;
+
+		if(ancestorIDs !== undefined) {
+			if(Object.prototype.toString.apply(ancestorIDs) !== '[object Array]') {
+				ancestorIDs = ancestorIDs.split(' ');
+			}
+			for(var aID in ancestorIDs) {
+				//var ancestor = MC.getComponent(ancestorIDs[id]);
+				//console.log(id, ancestor);
+				ancestors.push(ancestorIDs[aID]);
+				/*for(var key in ancestor.attrs) {
+					this[key] = ancestor.attrs[key];
+				}*/
+			}
+
+		}
 	};
-
-	//CONSTRUCTOR
-	this.getID = getID;
-	this.ancestors = ancestors;
-	this.attrs = attrs;
-
-	if(ancestorIDs !== undefined) {
-		if(Object.prototype.toString.apply(ancestorIDs) !== '[object Array]') {
-			ancestorIDs = ancestorIDs.split(' ');
-		}
-		for(var id in ancestorIDs) {
-			var ancestor = MC.getComponent(ancestorIDs[id]);
-			console.log(id, ancestor);
-			ancestors.push(ancestorIDs[id]);
-			/*for(var key in ancestor.attrs) {
-				this[key] = ancestor.attrs[key];
-			}*/
-		}
-
-	}
-};
 }(Maracuja);
--- src/entity.js
+++ src/entity.js
@@ -1,63 +1,64 @@
-
 var Entity = function(MC) {
-return function(c) {
-	//Private variables
-	var components = {};
+	return function(c) {
+		//Private variables
+		var components = {};
+		var self = this;
 
-	var implement = function(c) {
-		var pieces;
-		var com;
-		if(Object.prototype.toString.apply(c) === '[object Array]') {
-			pieces = c;
-		} else {
-			pieces = c.split(' ');
-		}
-		com = MC.getComponent(pieces[0]);
+		var implement = function(c) {
+			var pieces;
+			var com;
+			if(Object.prototype.toString.apply(c) === '[object Array]') {
+				pieces = c;
+			} else {
+				pieces = c.split(' ');
+			}
+			com = MC.getComponent(pieces[0]);
 
-		if(pieces.length > 1) {
-			for(var i = 0, len = pieces.length; i < len; i++) {
-				com = MC.getComponent(pieces[i]);
-				addComponent(com);
-			}
-			return this;
-		}
-		return addComponent(com)
-	};
-	
-	var addComponent = function(c) {
-		if(c && !has(c)) {
-			components[c.getID()] = c;
-			if(c.ancestors.length > 0) {
-				implement(c.ancestors);
-			}
-			var attrs = c.attrs;
-			if(attrs !== undefined) {
-				for(var key in attrs) {
-					this[key] = attrs[key];
+			if(pieces.length > 1) {
+				for(var i = 0, len = pieces.length; i < len; i++) {
+					com = MC.getComponent(pieces[i]);
+					addComponent(com);
 				}
-				if(attrs.init !== undefined) {
-					attrs.init();
-					this.init = undefined;
+				return self;
+			}
+			return addComponent(com)
+		};
+		
+		var addComponent = function(c) {
+			if(c && !has(c.getID())) {
+				components[c.getID()] = c;
+				if(c.ancestors.length > 0) {
+					implement(c.ancestors);
+				}
+				var attrs = c.attrs;
+				if(attrs !== undefined) {
+					for(var key in attrs) {
+						this[key] = attrs[key];
+					}
+					if(attrs.init !== undefined) {
+						attrs.init();
+						this.init = undefined;
+					}
 				}
 			}
-		}
+			return self;
+		};
+
+		var removeComponent = function(c) {
+			if(c && has(c.getID())) {
+				var com = MC.getComponent(c);
+
+			}
+		};
+		
+		var has = function(c) {
+			return !!components[c];
+		};
+
+		//ACTUAL CONSTRUCTOR
+		this.has = has;
+		this.implement = implement;
+		this.implement(c);
 		return this;
 	};
-
-	var removeComponent = function(c) {
-		if(c && has(c)) {
-//TODO
-		}
-	};
-	
-	var has = function(c) {
-		return !!components[c];
-	};
-
-	//ACTUAL CONSTRUCTOR
-	this.has = has;
-	this.implement = implement;
-	this.implement(c);
-	return this;
-};
 }(Maracuja);
