32617953783713c7c9c86fcf22d4c8c7ff05aa80
Robert Edstrom <robert@studsmedia.se>
2012-03-01 20:29:13 +0100

    made addCOMponent only call itself recursively

--- src/entity.js
+++ src/entity.js
@@ -30,7 +30,7 @@ var Entity = function(MC) {
 			} else {
 				addComponent(this, com);
 			}
-			handlers['init'].reverse();
+			handlers['init'].reverse(); //Usually event callbacks will be called in order of attachement - with onInit, the base ones get called first
 			this.trigger('init');
 			this.unbind('init');
 			return this;
