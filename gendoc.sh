012e08db954f7ea511181cbac08d5f1fee29bc43
Legogris <skit@legogris.se>
2012-04-19 19:17:16 +0200

    Started customizing template

--- gendoc.sh
+++ gendoc.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 # The location of your yuidoc install
 yuidoc_home=~/dev/js/yuidoc
-
+maracuja_dir=.
 # The location of the files to parse.  Parses subdirectories, but will fail if
 # there are duplicate file names in these directories.  You can specify multiple
 # source trees:
 #     parser_in="%HOME/www/yui/src %HOME/www/event/src"
-parser_in="$HOME/dev/js/lego/maracuja/src"
+parser_in=$maracuja_dir/src
 
 # The location to output the parser data.  This output is a file containing a 
 # json string, and copies of the parsed files.
-parser_out=~/dev/js/lego/maracuja/doc/parser
+parser_out=$maracuja_dir/doc/parser
 
 # The directory to put the html file outputted by the generator
-generator_out=~/dev/js/lego/maracuja/doc/generator
+generator_out=$maracuja_dir/doc/generator
 
 # The location of the template files.  Any subdirectories here will be copied
 # verbatim to the destination directory.
-template=$yuidoc_home/template
+#template=$yuidoc_home/template
+template=$maracuja_dir/doc/template
 
 # The version of your project to display within the documentation.
 version=0.0.1
@@ -29,4 +30,4 @@ yuiversion=2
 ##############################################################################
 # add -s to the end of the line to show items marked private
 
-$yuidoc_home/bin/yuidoc.py $parser_in -p $parser_out -o $generator_out -t $template -v $version
+$yuidoc_home/bin/yuidoc.py $parser_in -p $parser_out -o $generator_out -t $template -v $version -m Maracuja -u http://maracuja.cacti.se/
